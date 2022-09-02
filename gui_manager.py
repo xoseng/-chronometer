07721f13970fdf72688ea2b7604b48533132d721
jonathanAnguise <jonathan.anguise@protonmail.com>
2022-09-03 01:05:40 +0200

    continue to change it oriented object

--- gui_manager.py
+++ gui_manager.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Label, Button, Frame
+import time
 class Gui:
 
 	def __init__(self):
@@ -12,6 +13,58 @@ class Gui:
 
 		time['text'] = "00:00:00"
 
+		def start_chronometer(self, h=0, m=0, s=0):
+			global proceso
+
+			try:
+				stop_chronometer()
+			except:
+				pass
+
+			if s >= 60:
+				s = 0
+				m = m + 1
+				if m >= 60:
+					m = 0
+					h = h + 1
+					if h >= 24:
+						h = 0
+
+			if s < 10:
+				secss = '0' + str(s)
+			else:
+				secss = str(s)
+			if m < 10:
+				minss = '0' + str(m)
+			else:
+				minss = str(m)
+			if h < 10:
+				hourss = '0' + str(h)
+			else:
+				hourss = str(h)
+
+			time['text'] = hourss + ":" + minss + ":" + secss
+			# time['text'] = str(h) + ":" + str(m) + ":" + str(s)
+			proceso = time.after(1000, start_chronometer, (h), (m), (s + 1))
+
+		def stop_chronometer(self):
+			try:
+				global proceso
+				time.after_cancel(proceso)
+			except:
+				pass
+
+		def resume_chronometer(self):
+			try:
+				global proceso
+				hours = int(time['text'].split(':')[0])
+				mins = int(time['text'].split(':')[1])
+				secs = int(time['text'].split(':')[2]) + 1
+				stop_chronometer()
+				start_chronometer(h=hours, m=mins, s=secs)
+			except:
+				pass
+
 		frame = Frame(root)
 		btnIniciar = Button(frame, fg='green', text='Start', command=start_chronometer).grid(row=1, column=1,padx=2, pady=5)
 		btnParar = Button(frame, fg='red', text='Stop', command=stop_chronometer).grid(row=1, column=2,padx=2, pady=5)
@@ -23,54 +76,6 @@ class Gui:
 
 
 
-	def start_chronometer(self, h=0, m=0, s=0):
-	    global proceso
 
-	    try:
-		stop_chronometer()
-	    except:
-		pass
-
-	    if s >= 60:
-		s = 0
-		m = m + 1
-		if m >= 60:
-		    m = 0
-		    h = h + 1
-		    if h >= 24:
-			h = 0
-
-	    if s < 10:
-		secss='0'+str(s)
-	    else:
-		secss = str(s)
-	    if m < 10:
-		minss='0'+str(m)
-	    else:
-		minss =str(m)
-	    if h < 10:
-		hourss='0'+str(h)
-	    else:
-		hourss = str(h)
-		
-	    time['text'] = hourss + ":" + minss + ":" + secss
-	    #time['text'] = str(h) + ":" + str(m) + ":" + str(s)
-	    proceso = time.after(1000, start_chronometer, (h), (m), (s + 1))
-
-	def stop_chronometer(self):
-	    try:
-		global proceso
-		time.after_cancel(proceso)
-	    except:
-		pass
-
-	def resume_chronometer(self):
-	    try:
-		global proceso
-		hours=int(time['text'].split(':')[0])
-		mins=int(time['text'].split(':')[1])
-		secs=int(time['text'].split(':')[2])+1
-		stop_chronometer()
-		start_chronometer(h=hours, m=mins, s=secs)
-	    except:
-		pass
+if __name__ == "__main__":
+	my_gui = Gui()
