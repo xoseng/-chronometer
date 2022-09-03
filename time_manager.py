eef084f22d3583a064eb0818728ac193acebcb9e
jonathanAnguise <jonathan.anguise@protonmail.com>
2022-09-03 10:09:42 +0200

    create chrono function

--- time_manager.py
+++ time_manager.py
@@ -2,17 +2,25 @@ import time
 
 
 class Chrono:
-	def __init__(self):
+	def __init__(self, is_pause=False):
 		self.timer = time.perf_counter()
+		self.starting_time = self.timer
+		self.seconds = 0
+		self.is_pause = is_pause
+		self.seconds += 1
 
 	def end_chrono(self):
 		self.timer = time.perf_counter()
 
-
+	def update_time_spent_in_second(self):
+		if (time.perf_counter() % self.starting_time) > 1 and not self.is_pause:
+			print(self.seconds)
+			self.starting_time = time.perf_counter()
+			self.seconds += 1
+		time.sleep(0.25)
 
 if __name__ == "__main__":
+	time_duration = 20
 	my_chrono = Chrono()
-	print(f"{my_chrono.timer}")
-	time.sleep(3)
-	my_chrono.end_chrono()
-	print(f"{my_chrono.timer}")
+	while my_chrono.seconds <= time_duration:
+		my_chrono.update_time_spent_in_second()
