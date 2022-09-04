320d837b3a904679c7fde0fa3bca124d9893187f
jonathanAnguise <jonathan.anguise@protonmail.com>
2022-09-04 19:19:20 +0200

    correct some warning, change method display_time into function, create message when chrono ended

--- gui_manager.py
+++ gui_manager.py
@@ -1,4 +1,3 @@
-import tkinter
 from tkinter import Tk, Label, Button, Frame, Misc, Text, messagebox
 
 
@@ -9,6 +8,7 @@ class Chrono:
         self.process = None
         self.is_pause = is_pause
         self.max_time = 25 * 60
+
 
 class Gui:
 
@@ -26,24 +26,23 @@ class Gui:
 
     def create_main_window(self):
         self.time['text'] = "00:00"
-        self.end_timer = False
         frame = Frame(self.root)
-        self.btnIniciar = Button(frame, fg='green', text='Start', command=start_chronometer).grid(row=1,
-                                                                                                  column=1,
-                                                                                                  padx=2,
-                                                                                                  pady=5)
-        btnParar = Button(frame, fg='red', text='Stop', command=stop_chronometer).grid(row=1,
+        _btnStart = Button(frame, fg='green', text='Start', command=start_chronometer).grid(row=1,
+                                                                                            column=1,
+                                                                                            padx=2,
+                                                                                            pady=5)
+        _btnStop = Button(frame, fg='red', text='Stop', command=stop_chronometer).grid(row=1,
                                                                                        column=2,
                                                                                        padx=2,
                                                                                        pady=5)
-        btnReanudar = Button(frame, fg='blue', text='Resume', command=resume_chronometer).grid(row=1,
-                                                                                               column=3,
-                                                                                               padx=2,
-                                                                                               pady=5)
-        btnInput = Button(frame, fg='blue', text='Changing time', command=click_time_settings_window).grid(row=2,
-                                                                                                           column=2,
-                                                                                                           padx=2,
-                                                                                                           pady=5)
+        _btnResume = Button(frame, fg='blue', text='Resume', command=resume_chronometer).grid(row=1,
+                                                                                              column=3,
+                                                                                              padx=2,
+                                                                                              pady=5)
+        _btnInput = Button(frame, fg='blue', text='Changing time', command=click_time_settings_window).grid(row=2,
+                                                                                                            column=2,
+                                                                                                            padx=2,
+                                                                                                            pady=5)
         frame.pack()
 
     def create_time_settings_window(self):
@@ -63,12 +62,11 @@ class Gui:
     def main_loop(self):
         self.root.mainloop()
 
-    def display_time_in_mm_ss(self, time_in_seconds, max_time):
-        time_to_display = max_time - time_in_seconds
-        if time_to_display >= 0:
-            return '{:02d}:{:02d}'.format(time_to_display // 60, time_to_display % 60)
-        else:
-            self.end_timer = True
+
+def display_time_in_mm_ss(time_in_seconds, max_time):
+    time_to_display = max_time - time_in_seconds
+    return '{:02d}:{:02d}'.format(time_to_display // 60, time_to_display % 60)
+
 
 def start_chronometer():
     try:
@@ -76,9 +74,14 @@ def start_chronometer():
     except:
         pass
     my_chrono.process = my_gui.time.after(1000, start_chronometer)
-    print(my_chrono.process)
     my_chrono.seconds += 1
-    my_gui.time['text'] = my_gui.display_time_in_mm_ss(my_chrono.seconds, my_chrono.max_time)
+
+    if my_chrono.max_time == my_chrono.seconds:
+        my_gui.time.config(fg="red")
+        my_gui.time['text'] = "C'est fini"
+        stop_chronometer()
+    else:
+        my_gui.time['text'] = display_time_in_mm_ss(my_chrono.seconds, my_chrono.max_time)
 
 
 def stop_chronometer():
@@ -87,11 +90,13 @@ def stop_chronometer():
     except:
         pass
 
+
 def resume_chronometer():
     global my_gui
     stop_chronometer()
     my_chrono.seconds = 0
-    my_gui.time['text'] = my_gui.display_time_in_mm_ss(my_chrono.seconds, my_chrono.max_time)
+    my_gui.time['text'] = display_time_in_mm_ss(my_chrono.seconds, my_chrono.max_time)
+
 
 def get_value():
     global time_settings_window
@@ -100,14 +105,15 @@ def get_value():
         time = inp.split(":")
         max_time = int(time[0]) * 60 + int(time[1])
     except:
-        print("no Empty string please")
-        print("set default time 25 minutes")
+        time_settings_window.root.withdraw()
+        time_settings_window.root.deiconify()
         messagebox.showinfo('Error', 'format not correct\nCorrect format: xx:xx\nSet default time to 25 minutes')
         max_time = 25 * 60
         my_chrono.max_time = max_time
         return
     my_chrono.max_time = max_time
     time_settings_window.root.destroy()
+
 
 def click_time_settings_window():
     global time_settings_window
