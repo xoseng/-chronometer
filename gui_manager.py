81b7c50fda3e37a09f51f59a15e9e3d165a7984e
jonathanAnguise <jonathan.anguise@protonmail.com>
2022-09-04 18:39:56 +0200

    remove time_manager and include it into gui_manager

--- gui_manager.py
+++ gui_manager.py
@@ -1,7 +1,14 @@
 import tkinter
 from tkinter import Tk, Label, Button, Frame, Misc, Text, messagebox
-from time_manager import Chrono
 
+
+class Chrono:
+
+    def __init__(self, is_pause=False):
+        self.seconds = 0
+        self.process = None
+        self.is_pause = is_pause
+        self.max_time = 25 * 60
 
 class Gui:
 
--- time_manager.py
+++
@@ -1,9 +0,0 @@
-import time
-
-
-class Chrono:
-	def __init__(self, is_pause=False):
-		self.seconds = 0
-		self.process = None
-		self.is_pause = is_pause
-		self.max_time = 25 * 60
